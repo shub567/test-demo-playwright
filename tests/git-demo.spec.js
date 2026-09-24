2134cb9bd0854c2ea0feaad6803ab38a1d3286cc
shub567 <shubham567patel567@gmail.com>
2026-09-24 15:07:18 +0530

    Create conflicting test change

--- tests/git-demo.spec.js
+++ tests/git-demo.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Git demo test - conflict version A', async ({ page }) => {
+test('Git demo test - conflict version B', async ({ page }) => {
 //test('Git Demo Test', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(/Swag Labs/);
